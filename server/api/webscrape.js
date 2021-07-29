3d0186e395c7b697185128ebc11b6f6adafef1bf
yunggarlic <ferrari.tim.v@gmail.com>
2021-07-29 18:09:23 -0500

    edits replace regex

--- server/api/webscrape.js
+++ server/api/webscrape.js
@@ -1,55 +1,79 @@
 const router = require('express').Router();
 const puppeteer = require('puppeteer');
 const { parse } = require('node-html-parser');
+const redis = require('redis');
+const redisPort = process.env.REDIS_URL || 6379;
+const client = redis.createClient(redisPort);
 
 router.get('/', async (req, res) => {
   try {
-    //creates DOM Object from stringified html
-    const pageHtml = await getPageHtml('http://tygko.bandcamp.com');
-    const domObject = parse(pageHtml);
+    //redis db stores strings for 4 hours to reduce webscraping api calls
+    client.get('all', async (err, scrapedData) => {
+      if (err) throw err;
 
-    //grabs the music grid and filters li elements into array
-    const liArray = domObject
-      .querySelector('#music-grid')
-      .childNodes.filter((node) => {
-        return node.rawTagName === 'li';
-      });
-
-    const musicUrls = [];
-    const pictureUrls = [];
-    const releaseTitles = [];
-    liArray.forEach((htmlElement) => {
-      htmlElement.childNodes.forEach((node) => {
-        if (node.rawTagName === 'a') {
-          //shaves off "href= text and outer quotation and pushes url
-          musicUrls.push(
-            '/' + node.rawAttrs.slice(7, node.rawAttrs.length - 1)
-          );
-          node.childNodes
-            .filter((childNode) => {
-              return childNode.rawTagName;
-            })
-            .forEach((childNode) => {
-              childNode.childNodes.forEach((childNode) => {
-                if (childNode.rawTagName === 'p') {
-                  console.log(childNode);
-                }
-                if (childNode.rawTagName === 'img') {
-                  const regex = /\bsrc\S+jpg\b/;
-                  const result = childNode.rawAttrs.match(regex);
-                  pictureUrls.push(result[0].slice(5));
-                }
-              });
-            });
+      if (scrapedData) {
+        res.status(200).send(scrapedData);
+      } else {
+        //if the redis does not have anything already saved to it, run long webscrape
+        const data = await bandcampScrape();
+        let idx = 0;
+        for (let key in data) {
+          //setex(pire) = 4 hours before deletion
+          client.setex(idx, 14400, JSON.stringify(key));
+          idx++;
         }
-      });
+        client.setex('all', 14400, JSON.stringify(data));
+        await res.status(200).send(data);
+      }
     });
-    const urlObject = { musicUrls, pictureUrls };
-    res.send(urlObject);
   } catch (err) {
     console.error(err);
   }
 });
+
+const bandcampScrape = async () => {
+  //creates DOM Object from stringified html
+  const pageHtml = await getPageHtml('http://tygko.bandcamp.com');
+  const domObject = parse(pageHtml);
+
+  //grabs the artist homepage release grid and filters each li element into array
+  const liArray = domObject
+    .querySelector('#music-grid')
+    .childNodes.filter((node) => {
+      return node.rawTagName === 'li';
+    });
+
+  const musicUrls = [];
+  //scrapes each track url and stuffs into array for later use
+  liArray.forEach((htmlElement) => {
+    htmlElement.childNodes.forEach((node) => {
+      if (node.rawTagName === 'a') {
+        //shaves off "href= text and outer quotation and pushes url
+        musicUrls.push('/' + node.rawAttrs.slice(7, node.rawAttrs.length - 1));
+      }
+    });
+  });
+
+  const urlObject = { musicUrls, titles: [], pictures: [] };
+  //Takes each url from music grid and does the same as above
+  for (const url of musicUrls) {
+    const pageHtml = await getPageHtml(`http://tygko.bandcamp.com/${url}`);
+    const domObject = parse(pageHtml);
+    const title = domObject.querySelector('.trackTitle');
+    const pictures = domObject.querySelector('.popupImage');
+    if (title) {
+      urlObject.titles.push(
+        title.innerHTML
+          .replace(/^\s+|\s+$|\s+(?=\s)/g, '')
+          .replace(/\b&amp;\b/, '&')
+      );
+    }
+    if (pictures) {
+      urlObject.pictures.push(pictures._attrs.href);
+    }
+  }
+  return urlObject;
+};
 
 const getPageHtml = async (url) => {
   const browser = await puppeteer.launch();

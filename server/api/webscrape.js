68dcbdf0bb095e811f994dbbee3776de826c0dbd
yunggarlic <ferrari.tim.v@gmail.com>
2021-08-03 15:37:36 -0500

    updates browser launch

--- server/api/webscrape.js
+++ server/api/webscrape.js
@@ -77,7 +77,9 @@ const bandcampScrape = async () => {
 };
 
 const getPageHtml = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto(url);
   const pageHtml = await page.evaluate(

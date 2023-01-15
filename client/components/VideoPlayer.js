0a263b038d6edd07455b081db5593d9ce7fd2390
yunggarlic <ferrari.tim.v@gmail.com>
2023-01-14 21:22:38 -0600

    updates vid player

--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -3,12 +3,13 @@ import React from 'react';
 const VideoPlayer = () => {
   return (
     <iframe
-      className="video-player"
+      width="560"
+      height="315"
       src="https://www.youtube.com/embed/j1TzH8PA7qA"
       title="YouTube video player"
-      frameBorder="0"
+      frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
+      allowfullscreen
     ></iframe>
   );
 };

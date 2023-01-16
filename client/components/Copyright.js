c3d83acedf9f1227eeec44dfcce74283d6dfba2c
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-01-16 09:26:52 -0600

    updates copyright year

--- client/components/Copyright.js
+++ client/components/Copyright.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Copyright = () => {
   return (
     <div className="copyright">
-      <p style={{ fontSize: '.75rem' }}>Copyright 2021 tygko</p>
+      <p style={{ fontSize: '.75rem' }}>Copyright 2023 tygko</p>
       <p style={{ fontSize: '.75rem' }}>all rights reserved or whatever</p>
     </div>
   );
